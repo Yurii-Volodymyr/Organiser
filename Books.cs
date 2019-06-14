5c00ce1db789f24947dd32155925c54a2b2dff41
Yurii-Volodymyr <32770315+Yurii-Volodymyr@users.noreply.github.com>
2019-06-14 10:58:53 +0200

    Books SQL

--- Books.cs
+++ Books.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace ToDoList
 {
@@ -16,6 +17,7 @@ namespace ToDoList
         public Books()
         {
             InitializeComponent();
+            getTargets();
         }
         public void addItem(string text)
         {
@@ -31,8 +33,38 @@ namespace ToDoList
             //add item button
             string tarName = textBox.Text;
             addItem(tarName);
+            addTargets(tarName);
             textBox.Text = "";
 
         }
+        void getTargets()
+        {
+            SqlConnection SQL = new SqlConnection(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Targets;Integrated Security=True;Pooling=False");
+
+            using (SQL)
+            {
+                SQL.Open();
+                SqlCommand command = new SqlCommand("select * from TargetTab where TargetID=2", SQL);
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        addItem("" + reader["TargetText"]);
+                    }
+                }
+            }
+        }
+
+        public void addTargets(string insert)
+        {
+            SqlConnection SQL = new SqlConnection(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Targets;Integrated Security=True;Pooling=False");
+            using (SQL)
+            {
+                SQL.Open();
+                SqlCommand commandSec = new SqlCommand("insert into TargetTab(TargetText, TargetID), values('" + insert + "', '2')", SQL);
+                commandSec.ExecuteNonQuery();
+            }
+        }
+
     }
 }
